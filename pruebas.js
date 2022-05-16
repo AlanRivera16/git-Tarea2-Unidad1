34792797601260a74ee0d8c2c37ef76badddefd8
unknown <emporium@GEIT204.grupoemporium.com.mx>
2022-05-16 14:39:37 -0500

    added: Our first file was added

---
+++ pruebas.js
@@ -0,0 +1,7 @@
+Leyenda = {
+    nombre: "Mirage",
+    pasiva: "Camuflaje al revivir - reaparecer",
+    tactica: "Clon señuelo",
+    ultima: "Ejercito de clones",
+    daño_inflingido: 3495892
+}
